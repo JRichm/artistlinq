3e72ba3411e5691d2761e7651602566477cabff0
JRichm <jblackenmusic@gmail.com>
2023-06-30 22:18:55 -0600

    built out model.py file for postgreSQL server

---
+++ model.py
@@ -0,0 +1,106 @@
+""""""""""""""""""""""""""""""""""""""""""
+"""  ###  PostgreSQL  Model  File ###  """
+""""""""""""""""""""""""""""""""""""""""""
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+db = SQLAlchemy()
+
+
+""""""""""""""""""""""""""""""""""""""""""
+""" ###      PostgreSQL Tables     ### """
+""""""""""""""""""""""""""""""""""""""""""
+
+""" Users Table """
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.VARCHAR, unique=True, nullable=False)
+    email = db.Column(db.VARCHAR, unique=True, nullable=False)
+    password_hash = db.Column(db.VARCHAR, nullable=False)
+    created_at = db.Column(db.TIMESTAMP)
+    updated_at = db.Column(db.TIMESTAMP)
+
+
+""" Image Table """
+class Image(db.Model):
+    __tablename__ = 'images'
+    
+    image_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    image_url = db.Column(db.VARCHAR, unique=True, nullable=False)
+    caption = db.Column(db.VARCHAR)
+    created_at = db.Column(db.TIMESTAMP)
+    updated_at = db.Column(db.TIMESTAMP)
+
+
+""" Comment Table """
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    
+    comment_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    image_id = db.Column(db.Integer, db.ForeignKey('images.image_id'), nullable=False)
+    comment_text = db.Column(db.VARCHAR)
+    created_at = db.Column(db.TIMESTAMP)
+    updated_at = db.Column(db.TIMESTAMP)
+    
+
+""" Commission Request Table """
+class CommissionRequest(db.Model):
+    __tablename__ = 'commissionRequests'
+    
+    request_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    request_text = db.Column(db.VARCHAR)
+    status = db.Column(db.VARCHAR)
+    created_at = db.Column(db.TIMESTAMP)
+    updated_at = db.Column(db.TIMESTAMP)
+
+""" Artist Table """
+class Artists(db.Model):
+    __tablename__ = 'Artists'
+    
+    artist_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    bio = db.Column(db.TEXT)
+    skills = db.Column(db.ARRAY(db.VARCHAR))
+    created_at = db.Column(db.TIMESTAMP)
+    updated_at = db.Column(db.TIMESTAMP)
+
+
+""" Like Table """
+class Likes(db.Model):
+    __tablename__ = 'likes'
+    
+    like_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    image_id = db.Column(db.Integer, db.ForeignKey('images.image_id'), nullable=False)
+    created_at = db.Column(db.TIMESTAMP)
+    
+
+""""""""""""""""""""""""""""""""""""""""""
+""" ###       Database Config      ### """
+""""""""""""""""""""""""""""""""""""""""""
+
+def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["POSTGRES_URI"]
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+
+if __name__ == "__main__":
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
--- server.py
+++ server.py
@@ -6,23 +6,29 @@ import forms
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dev'
 
+
 """ ### Flask Routes ### """
 
-### " View Homepage " ###
+    ### " View Homepage " ###
+    
 @app.route('/')
 def index():
     return render_template('index.html')
   
-### " View Login/New User" ###
+  
+    ### " View Login/New User" ###
+    
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     loginForm = forms.LoginForm()
     newUserForm = forms.RegistrationForm()
     if loginForm.validate_on_submit() or newUserForm.validate_on_submit():
         # Process login form submission
+        print('fart')
         # Perform authentication and redirect
         return redirect(url_for('index'))
     return render_template('login.html', loginForm=loginForm, newUserForm=newUserForm)
+
 
 """ ### Server Methods ### """
 
