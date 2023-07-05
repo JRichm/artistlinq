f4f8cfe462c173174b094704a5b189e7918a2cd6
JRichm <jblackenmusic@gmail.com>
2023-07-05 12:48:25 -0600

    added user_view page

--- forms.py
+++ forms.py
@@ -1,17 +1,60 @@
 """ ### Flask Forms ### """
 
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import flash, redirect, url_for, session
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
+import crud
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()], render_kw={"placeholder": "Username"})
     password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
     submit = SubmitField('Login')
     
+    def login(self):
+        print('Process login form submission')
+        if self.validate_on_submit():
+            # Process login form submission
+            username = self.username.data
+            password = self.password.data
+            user = crud.get_user_by_username(username)
+            if user and check_password_hash(user.password_hash, password):
+                # Authentication successful
+                session['username'] = username
+                return redirect(url_for('index'))
+            else:
+                flash('invalid deets')
+                # flash('Invalid username or password. Please try again.')
+    
+        return redirect(url_for('login'))  # Return a valid response
+
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()], render_kw={"placeholder": "Username"})
     email = StringField('Email', validators=[DataRequired(), Email()], render_kw={"placeholder": "Email"})
     password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "Password"})
     confirm_password = PasswordField('Password', validators=[DataRequired(), EqualTo('password')], render_kw={"placeholder": "Confirm Password"})
     submit = SubmitField('Create Account')
+    
+    def create_user(self):
+        print('Process new user form submission')
+        if self.validate_on_submit():
+            # Process new user form submission
+            username = self.username.data
+            password = self.password.data
+            email = self.email.data
+            if crud.get_user_by_username(username):
+                flash('Username already exists. Please choose a different username.')
+            else:
+                # Generate password hash with salt
+                crud.create_user(
+                    username=username,
+                    password=password,
+                    email=email
+                )
+                session['username'] = username
+                flash('Account created successfully!')
+                return redirect(url_for('index'))
+        
+        return redirect(url_for('login'))  # Return a valid response
--- server.py
+++ server.py
@@ -5,7 +5,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from model import connect_to_db, db
-import controller
 import forms
 import crud
 import os
@@ -16,10 +15,40 @@ app.config['SECRET_KEY'] = 'dev'
 """"""""""""""""""""""""""""""""""""""""""
 """     ###    Flask Routes    ###     """
 """"""""""""""""""""""""""""""""""""""""""
+
     ### " View Homepage " ###
-    
 @app.route('/')
 def index():
+    return render_template('index.html', images=get_images())
+  
+  
+    ### " View Login/New User" ###
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    loginForm = forms.LoginForm()
+    newUserForm = forms.RegistrationForm()
+    
+    if request.form.get('login') == 'Login':
+        return loginForm.login()
+        
+    elif request.form.get('new-user') == 'Create Account':
+        return newUserForm.create_user()
+        
+    return render_template('login.html', loginForm=loginForm, newUserForm=newUserForm)
+  
+
+    ### " View User " ###
+@app.route('/user/<username>')
+def user_view(username):
+    user = crud.get_user_by_username(username)
+    return render_template('user_view.html', user=user, images=get_images())
+
+
+""""""""""""""""""""""""""""""""""""""""""
+"""  ###     Server Methods     ###    """
+""""""""""""""""""""""""""""""""""""""""""
+    ### " Temporary images " ###
+def get_images():
     image_folder = './static/images/'
     images = []
     
@@ -29,29 +58,8 @@ def index():
             image_url = '/static/images/' + filename
             images.append({'path': image_path, 'url': image_url})
             
-    
-    return render_template('index.html', images=images)
-  
-  
-    ### " View Login/New User" ###
-    
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    loginForm = forms.LoginForm()
-    newUserForm = forms.RegistrationForm()
-    
-    if request.form.get('login') == 'Login':
-        return controller.login(loginForm)
-        
-    elif request.form.get('new-user') == 'Create Account':
-        return controller.create_user(newUserForm)
-        
-    return render_template('login.html', loginForm=loginForm, newUserForm=newUserForm)
+    return images
 
-
-""""""""""""""""""""""""""""""""""""""""""
-"""  ###     Server Methods     ###    """
-""""""""""""""""""""""""""""""""""""""""""
 if __name__ == "__main__":
     connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
