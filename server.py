220d544072ce63e428a0eeb63244a67cfca12cab
JRichm <jblackenmusic@gmail.com>
2023-07-24 17:23:03 -0600

    updated app.py

--- server.py
+++ server.py
@@ -410,4 +410,4 @@ def flash_errors(form):
             flash(f"Error in field '{getattr(form, field).label.text}': {error}", "error")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=8080)
