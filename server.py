e09cc2ae94445f267ed9714e2c40e5decf93723b
JRichm <jblackenmusic@gmail.com>
2023-07-24 12:55:01 -0600

    removed modules

--- server.py
+++ server.py
@@ -4,7 +4,6 @@
 """"""""""""""""""""""""""""""""""""""""""
 """      ### Server Imports ###        """
 """"""""""""""""""""""""""""""""""""""""""
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Flask, render_template, redirect, request, url_for, flash, jsonify, session
 from flask_sqlalchemy import SQLAlchemy
 from app import app
