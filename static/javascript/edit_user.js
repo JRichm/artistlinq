cc85dabcd01037a168fc227246cb2bbe87ca0308
JRichm <jblackenmusic@gmail.com>
2023-07-31 16:27:35 -0600

    added edit user js

--- static/javascript/edit_user.js
+++ static/javascript/edit_user.js
@@ -1,1 +1,11 @@
 uploadedIMG = document.getElementById('uploaded-icon');
+let dragging = false;
+
+
+uploadedIMG.addEventListener('mousedown', e => {
+    console.log('onmousedown')
+})
+document.addEventListener('mouseup', e => {
+    console.log('Not dragging')
+    dragging = false;
+})
