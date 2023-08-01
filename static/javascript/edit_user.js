0ca23f7437c108409264d0ca290375685c083c1e
JRichm <jblackenmusic@gmail.com>
2023-08-01 14:31:36 -0600

    user can drag picture

--- static/javascript/edit_user.js
+++ static/javascript/edit_user.js
@@ -1,3 +1,4 @@
+let fileInput = document.getElementById('new-icon-input')
 let uploadedIMG = document.getElementById('uploaded-icon');
 let iconContainer = document.getElementById('new-icon-box');
 let dragging = false;
@@ -6,8 +7,25 @@ let clickPos = [0, 0];
 let diff = [0, 0];
 let imageSize = [0, 0];
 
+// when user uploads picture file
+fileInput.addEventListener('change', e => {
+    let diff = [0, 0];
+    let clickPos = [0, 0];
+    if (fileInput.files && fileInput.files[0]) {
+        const reader = new FileReader();
 
-// when user clicks
+        reader.onload = e => {
+            uploadedIMG.src = e.target.result;
+            imageSize = [uploadedIMG.offsetWidth, uploadedIMG.offsetHeight] // add this to the input event listener
+                                                                    // only need to get the size once each time
+        }
+
+        reader.readAsDataURL(fileInput.files[0]);
+    }
+    
+})
+
+// when user clicks picture
 uploadedIMG.addEventListener('mousedown', e => {
     clickPos = [e.clientX - diff[0], e.clientY - diff[1]]
     dragging = true
@@ -15,7 +33,7 @@ uploadedIMG.addEventListener('mousedown', e => {
                                                                     // only need to get the size once each time the user uploads
 })
 
-// when user drags
+// when user drags picture
 window.addEventListener('mousemove', e => {
     if (dragging) {
         mousePos = [e.clientX, e.clientY]
@@ -26,11 +44,11 @@ window.addEventListener('mousemove', e => {
         if (diff[0] < -imageSize[0] + containerSize[0]) diff[0] = -imageSize[0] + containerSize[0]
         if (diff[1] < -imageSize[1] + containerSize[1]) diff[1] = -imageSize[1] + containerSize[1]
 
-        uploadedIMG.style = `transform: translateX(${diff[0]}px) translateY(${diff[1]}px`
+        uploadedIMG.style = `transform: translateX(${diff[0]}px) translateY(${diff[1]}px);`
     }
 })
 
-// when user releases click
+// when user releases click 
 document.addEventListener('mouseup', e => {
     dragging = false;
 })
