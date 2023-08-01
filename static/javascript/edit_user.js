b20174c40177c4c0d03590fd2633a4b003f36c2d
JRichm <jblackenmusic@gmail.com>
2023-08-01 13:30:44 -0600

    user can move picture after upload

--- static/javascript/edit_user.js
+++ static/javascript/edit_user.js
@@ -1,16 +1,36 @@
-uploadedIMG = document.getElementById('uploaded-icon');
+let uploadedIMG = document.getElementById('uploaded-icon');
+let iconContainer = document.getElementById('new-icon-box');
 let dragging = false;
+let containerSize = [iconContainer.offsetWidth, iconContainer.offsetHeight];
+let clickPos = [0, 0];
+let diff = [0, 0];
+let imageSize = [0, 0];
 
 
+// when user clicks
 uploadedIMG.addEventListener('mousedown', e => {
-    console.log('onmousedown')
+    clickPos = [e.clientX - diff[0], e.clientY - diff[1]]
+    dragging = true
+    imageSize = [uploadedIMG.offsetWidth, uploadedIMG.offsetHeight] // add this to the input event listener
+                                                                    // only need to get the size once each time the user uploads
 })
 
+// when user drags
 window.addEventListener('mousemove', e => {
-    
+    if (dragging) {
+        mousePos = [e.clientX, e.clientY]
+        diff = [mousePos[0] - clickPos[0], mousePos[1] - clickPos[1]]
+
+        if (diff[0] > 0) diff[0] = 0
+        if (diff[1] > 0) diff[1] = 0
+        if (diff[0] < -imageSize[0] + containerSize[0]) diff[0] = -imageSize[0] + containerSize[0]
+        if (diff[1] < -imageSize[1] + containerSize[1]) diff[1] = -imageSize[1] + containerSize[1]
+
+        uploadedIMG.style = `transform: translateX(${diff[0]}px) translateY(${diff[1]}px`
+    }
 })
 
+// when user releases click
 document.addEventListener('mouseup', e => {
-    console.log('Not dragging')
     dragging = false;
 })
