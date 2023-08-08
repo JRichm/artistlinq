31b73700c3859708441609a3e449f9d480bffeed
JRichm <jblackenmusic@gmail.com>
2023-08-08 14:07:42 -0600

    got cropper to work

--- static/javascript/edit_user.js
+++ static/javascript/edit_user.js
@@ -1,5 +1,32 @@
-const image = document.getElementById('image')
-const cropper = new Cropper(image, {
-    aspectRatio: 0,
-    viewMode: 0,
-});
+const newIconIMGElement = document.getElementById('image')
+
+const iconInput = document.getElementById('new-icon-input');
+iconInput.addEventListener('change', e => { 
+    const file = e.target.files[0]
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            newIconIMGElement.src = e.target.result;
+            newIconIMGElement.onload = initCropper;
+        }
+        reader.readAsDataURL(file);
+    }
+})
+
+let cropper
+
+function initCropper() {
+    cropper = new Cropper(image, {
+        aspectRatio: 0,
+        viewMode: 0,
+    })
+}
+
+document.getElementById('cropImageBtn').addEventListener('click', e => {
+    e.preventDefault() 
+    var croppedImage = cropper.getCroppedCanvas().toDataURL("image/png");
+
+    document.getElementById('output').src = croppedImage
+
+    alert(document.getElementById('output').src)
+})
