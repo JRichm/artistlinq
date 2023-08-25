8d8da702d0d63bc3e51d68ae868b2d8e67e3c1e2
JRichm <jblackenmusic@gmail.com>
2023-08-25 12:29:02 -0600

    added gunicorn to sudo install

--- init_server.sh
+++ init_server.sh
@@ -1,7 +1,6 @@
 sudo apt update
 sudo apt update
 
-sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools gunicorn
 
 pip install -r requirements.txt
-pip install gunicorn
