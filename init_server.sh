1b411b3ee911bcc62a0901d0d4bd228eeb6c3e66
JRichm <jblackenmusic@gmail.com>
2023-08-24 21:27:32 -0600

    updated init_server.sh

--- init_server.sh
+++ init_server.sh
@@ -2,7 +2,6 @@ sudo apt update
 sudo apt update
 
 pip install -r requirements.txt
-pip uninstall gunicorn
-pip install gunicorn
+export PATH=$PATH:/usr/local/python3/bin && pip install gunicorn && gunicorn server:app
 
 cat requirements.txt
