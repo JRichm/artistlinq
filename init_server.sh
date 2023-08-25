884fa533e6643235204769f1d480ab1193e60f6b
JRichm <jblackenmusic@gmail.com>
2023-08-25 12:22:22 -0600

    updated init_server

--- init_server.sh
+++ init_server.sh
@@ -1,4 +1,7 @@
 sudo apt update
 sudo apt update
 
+sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+
 pip install -r requirements.txt
+pip install gunicorn
